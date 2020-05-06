828c83b6129537fd727525c17020b2f835dc3513
Aurelien Cerveaux <a.cerveaux@betclicgroup.com>
2020-05-07 00:41:36 +0200

    Tutorial Part 3

--- SoftEngine/Device.cs
+++ SoftEngine/Device.cs
@@ -1,6 +1,11 @@
-﻿using SharpDX;
+﻿using Newtonsoft.Json;
+using SharpDX;
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace SoftEngine
@@ -113,9 +118,7 @@ namespace SoftEngine
         {
             // Clipping what's visible on screen
             if (point.X >= 0 && point.Y >= 0 && point.X < _bmp.PixelWidth && point.Y < _bmp.PixelHeight)
-            {
                 PutPixel((int)point.X, (int)point.Y, Color.Yellow);
-            }
         }
 
         /// <summary>
@@ -185,9 +188,7 @@ namespace SoftEngine
             foreach (Mesh mesh in meshes)
             {
                 // Beware to apply rotation before translation
-                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y,
-                                                              mesh.Rotation.X, mesh.Rotation.Z) *
-                                  Matrix.Translation(mesh.Position);
+                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y, mesh.Rotation.X, mesh.Rotation.Z) * Matrix.Translation(mesh.Position);
 
                 var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
 
@@ -227,6 +228,80 @@ namespace SoftEngine
                     //DrawBLine(pixelC, pixelA);
                 }
             }
+        }
+
+        /// <summary>
+        /// Loads the json file asynchronous from a babylon export in blender
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>An array of mesh</returns>
+        public async Task<Mesh[]> LoadJsonFileAsync(string fileName)
+        {
+            var meshes = new List<Mesh>();
+            var file = await Package.Current.InstalledLocation.GetFileAsync(fileName);
+            var data = await FileIO.ReadTextAsync(file);
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(data);
+
+            for (var meshIndex = 0; meshIndex < jsonObject.meshes.Count; meshIndex++)
+            {
+                var verticesArray = jsonObject.meshes[meshIndex].vertices;
+
+                // Faces
+                var indicesArray = jsonObject.meshes[meshIndex].indices;
+
+                var uvCount = jsonObject.meshes[meshIndex].uvCount.Value;
+                var verticesStep = 1;
+
+                // Depending of the number of texture's coordinates per vertex
+                // we're jumping in the vertices array  by 6, 8 & 10 windows frame
+                switch ((int)uvCount)
+                {
+                    case 0:
+                        verticesStep = 6;
+                        break;
+
+                    case 1:
+                        verticesStep = 8;
+                        break;
+
+                    case 2:
+                        verticesStep = 10;
+                        break;
+                }
+
+                var verticesCount = verticesArray.Count / verticesStep;
+
+                // number of faces is logically the size of the array divided by 3 (A, B, C)
+                var facesCount = indicesArray.Count / 3;
+
+                var mesh = new Mesh(jsonObject.meshes[meshIndex].name.Value, verticesCount, facesCount);
+
+                // Filling the Vertices array of our mesh first
+                for (var index = 0; index < verticesCount; index++)
+                {
+                    var x = (float)verticesArray[index * verticesStep].Value;
+                    var y = (float)verticesArray[index * verticesStep + 1].Value;
+                    var z = (float)verticesArray[index * verticesStep + 2].Value;
+                    mesh.Vertices[index] = new Vector3(x, y, z);
+                }
+
+                // Then filling the Faces array
+                for (var index = 0; index < facesCount; index++)
+                {
+                    var a = (int)indicesArray[index * 3].Value;
+                    var b = (int)indicesArray[index * 3 + 1].Value;
+                    var c = (int)indicesArray[index * 3 + 2].Value;
+                    mesh.Faces[index] = new Face { A = a, B = b, C = c };
+                }
+
+                // Getting the position you've set in Blender
+                var position = jsonObject.meshes[meshIndex].position;
+                mesh.Position = new Vector3((float)position[0].Value, (float)position[1].Value, (float)position[2].Value);
+                meshes.Add(mesh);
+            }
+
+            return meshes.ToArray();
         }
     }
 }
--- SoftEngine/MainPage.xaml.cs
+++ SoftEngine/MainPage.xaml.cs
@@ -14,7 +14,7 @@ namespace SoftEngine
     public sealed partial class MainPage : Page
     {
         private Device _device;
-        private Mesh _mesh = new Mesh("Cube", 8, 12);
+        private Mesh[] _meshes;
         private Camera _camera = new Camera();
 
         public MainPage()
@@ -22,7 +22,7 @@ namespace SoftEngine
             this.InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // Get the screen size
             //var displayInformation = Windows.Graphics.Display.DisplayInformation.GetForCurrentView();
@@ -30,32 +30,12 @@ namespace SoftEngine
 
             WriteableBitmap bmp = new WriteableBitmap(640, 480);
 
-            _device = new Device(bmp);
-
             // Image XAML Control
             frontBuffer.Source = bmp;
 
-            _mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            _mesh.Vertices[1] = new Vector3(1, 1, 1);
-            _mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            _mesh.Vertices[3] = new Vector3(1, -1, 1);
-            _mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            _mesh.Vertices[5] = new Vector3(1, 1, -1);
-            _mesh.Vertices[6] = new Vector3(1, -1, -1);
-            _mesh.Vertices[7] = new Vector3(-1, -1, -1);
+            _device = new Device(bmp);
 
-            _mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            _mesh.Faces[1] = new Face { A = 0, B = 1, C = 4 };
-            _mesh.Faces[2] = new Face { A = 0, B = 2, C = 7 };
-            _mesh.Faces[3] = new Face { A = 0, B = 4, C = 7 };
-            _mesh.Faces[4] = new Face { A = 1, B = 2, C = 3 };
-            _mesh.Faces[5] = new Face { A = 1, B = 3, C = 6 };
-            _mesh.Faces[6] = new Face { A = 1, B = 5, C = 6 };
-            _mesh.Faces[7] = new Face { A = 1, B = 4, C = 5 };
-            _mesh.Faces[8] = new Face { A = 2, B = 3, C = 7 };
-            _mesh.Faces[9] = new Face { A = 3, B = 6, C = 7 };
-            _mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            _mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
+            _meshes = await _device.LoadJsonFileAsync("monkey.babylon");
 
             _camera.Position = new Vector3(0, 0, 10.0f);
             _camera.Target = Vector3.Zero;
@@ -67,11 +47,12 @@ namespace SoftEngine
         {
             _device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            _mesh.Rotation = new Vector3(_mesh.Rotation.X + 0.01f, _mesh.Rotation.Y + 0.01f, _mesh.Rotation.Z);
+            foreach (var mesh in _meshes)
+                mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
 
             // Doing the various matrix operations
-            _device.Render(_camera, _mesh);
+            _device.Render(_camera, _meshes);
+
             // Flushing the back buffer into the front buffer
             _device.Present();
         }
